1b7eb6a9b7343cd9dd88a96bcb035d62ba26dbb0
wallaceeBenites <wallacepuck@gmail.com>
2025-05-15 22:20:36 -0300

    teste

--- manipular_vetores.v
+++ manipular_vetores.v
@@ -2,7 +2,9 @@ module manipular_vetores(
     input [31:0] entrada,
     output [31:0] saida );
 
- assign saida = {entrada[7:0], entrada[15:8], entrada[23:16], entrada[31:24]};
+    always @(*) begin
+        saida = {entrada[7:0], entrada[15:8], entrada[23:16], entrada[31:24]};
+    end
 
 
 endmodule
