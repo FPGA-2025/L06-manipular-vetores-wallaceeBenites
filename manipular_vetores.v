79894ca802eae7a1fcccd8f890e8aa9949712bdf
wallaceeBenites <wallacepuck@gmail.com>
2025-05-15 22:44:55 -0300

    testes

--- manipular_vetores.v
+++ manipular_vetores.v
@@ -2,7 +2,10 @@ module manipular_vetores(
     input [31:0] entrada,
     output [31:0] saida );
 
-  assign saida = {entrada[7:0], entrada[15:8], entrada[23:16], entrada[31:24]};
+assign saida[31:24] = entrada[7:0];   // Byte 0 da entrada vai para o Byte 3 da saída
+assign saida[23:16] = entrada[15:8];  // Byte 1 da entrada vai para o Byte 2 da saída
+assign saida[15:8]  = entrada[23:16]; // Byte 2 da entrada vai para o Byte 1 da saída
+assign saida[7:0]   = entrada[31:24]; // Byte 3 da entrada vai para o Byte 0 da saída
 
 
 endmodule
